a7aa03b3559e7dfb750f263c281f9de6adc9ef7a
A1exxuss <32575876+dedushkaalex@users.noreply.github.com>
2023-08-02 22:08:37 +0300

    build: setup prettier and setting prettier configurations (#13)
    Authored-by: Aleksei Erchenko <proger@a1exxuss.ru>

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App.tsx';
 
 const router = createBrowserRouter([
@@ -13,5 +14,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
