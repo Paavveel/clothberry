47f715a84da4d38db53bdf8e5d539c27bf2c680b
Aleksei Erchenko <proger@a1exxuss.ru>
2023-08-13 01:09:40 +0300

    feat: (CLOTHBERRY-19) add Form component

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+
+export const Layout: FC = () => {
+  return (
+    <div className='app container'>
+      <main className='main'>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
