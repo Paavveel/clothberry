84540194d8e880abee9c67fd57150321eae6c1b8
Aleksei Erchenko <proger@a1exxuss.ru>
2023-08-13 01:05:09 +0300

    feat: (CLOTHBERRY-19) add base routing for register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
-const App = () => <div>Hello to the Clothberry</div>;
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Layout } from '@layout/Layout';
+import { RegisterPage } from '@pages/Register/RegisterPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <h1>Main</h1>,
+        index: true,
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
