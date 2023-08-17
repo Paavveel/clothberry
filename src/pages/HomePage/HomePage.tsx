011cb6adbde8b58c716315e557980bd5b4dcaa93
Vladimir <noise123q@mail.ru>
2023-08-17 22:13:11 +0300

    feat: (CLOTHBERRY-20) add homePage and notFoundPage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,22 @@
+import classes from './HomePage.module.css';
+
+export const HomePage = () => {
+  return (
+    <div className={classes.wrapper}>
+      <header className={classes.header}>
+        <ul className={classes.header__items}>
+          <li className={classes.header__item}>Новинки</li>
+          <li className={classes.header__item}>Одежда</li>
+          <li className={classes.header__item}>Аксессуары</li>
+          <li className={classes.header__item}>SALE</li>
+        </ul>
+        <a href='/'>
+          <img src='./src/assets/images/logo.png' alt='logo' />
+        </a>
+        <input type='text' />
+        <button type='button'>login</button>
+        <button type='button'>registration</button>
+      </header>
+    </div>
+  );
+};
