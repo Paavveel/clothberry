e75c7d119d098b7d8f72957a0c7108b48a84cde0
Vladimir <noise123q@mail.ru>
2023-08-19 14:32:37 +0300

    feat: (CLOTHBERRY-20) add style for not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,18 @@
-import classes from './NotFoundPage.module.css';
+import { Link } from 'react-router-dom';
+
+import styles from './NotFoundPage.module.css';
 
 export const NotFoundPage = () => {
   return (
-    <div className='app container'>
-      <h1 className={classes.title}>404 Not Found</h1>
-      <span>The address was typed incorrectly, or the page no longer exists on the site.</span>
+    <div className={styles.wrapper}>
+      <span className={styles.numbers}>404</span>
+      <h1 className={styles.title}>Page Not Found</h1>
+      <span className={styles.subtitle}>
+        The address was typed incorrectly, or the page no longer exists on the site.
+      </span>
+      <Link className={styles['btn-home']} to='/'>
+        Back to Home
+      </Link>
     </div>
   );
 };
