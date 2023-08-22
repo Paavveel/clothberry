623b6865b36d09bfea15a1414aac626d0b9c58eb
Paavveel <pa.tomilin@gmail.com>
2023-08-22 23:45:47 +0300

    feat: add tests for authSlice

---
+++ src/store/features/auth/authSlice.test.ts
@@ -0,0 +1,25 @@
+import AuthReducer, { AuthState, clearError, logout } from './authSlice';
+
+describe('authSlice', () => {
+  const initialState: AuthState = {
+    isLoggedIn: false,
+    customer: null,
+    loading: false,
+    errorMessage: '',
+  };
+
+  it('should handle initial state', () => {
+    expect(AuthReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+  });
+
+  it('should handle logout', () => {
+    const actual = AuthReducer(initialState, logout());
+    expect(actual.isLoggedIn).toBeFalsy();
+    expect(actual.customer).toBeNull();
+  });
+
+  it('should handle clearError', () => {
+    const actual = AuthReducer(initialState, clearError());
+    expect(actual.errorMessage).toEqual('');
+  });
+});
