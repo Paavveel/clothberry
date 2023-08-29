17fead649b67f8f93aa66bc1b0a1bcdec4f98eae
Paavveel <pa.tomilin@gmail.com>
2023-08-29 23:52:07 +0300

    feat: (CLOTHBERRY-40) add toast

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
+
+import { toastOptions } from 'config/toastOptions';
 
 import { AppWrapper } from '@components/AppWrapper';
 import { Header } from '@components/Header/Header';
@@ -13,6 +16,7 @@ export const Layout: FC = () => {
           <Outlet />
         </main>
       </AppWrapper>
+      <Toaster position='bottom-right' toastOptions={toastOptions} />
     </div>
   );
 };
