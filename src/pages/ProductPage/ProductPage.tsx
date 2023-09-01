b85163c3240de58f2e36f8313a9d7b39fff11b79
Vladimir <noise123q@mail.ru>
2023-09-01 12:38:47 +0300

    feat: (CLOTHBERRY-41) title, description and price

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { api } from '@api/client';
+
 import classes from './ProductPage.module.css';
 
 export const ProductPage = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetch = async (id: string) => {
+      let response;
+      try {
+        response = await api.request.productProjections().withId({ ID: id }).get().execute();
+        return response.body;
+      } catch (error) {
+        console.error('Error fetching product:', error);
+        return false;
+      }
+    };
+    fetch('9de39ac5-f559-4e53-a82f-4d5eeb4457f8').then((response) => setData(response));
+  }, []);
+
+  const price = data.masterVariant?.prices[0].value.centAmount;
+  const currentPrice = price / 100;
+  const code = data.masterVariant?.prices[0].value.currencyCode;
   return (
-    <div className={classes.product}>
+    <section className={classes.product}>
       <div className={classes.img}> </div>
       <div className={classes.content}>
-        <span className={classes.title}>Название товара</span>
-        <p className={classes.description}>Описание товара</p>
+        <h1 className={classes.title}>{data.name?.en}</h1>
+        <span className={classes.price}>{`${currentPrice} ${code}`}</span>
+        <p className={classes.description}>{data.description?.en}</p>
       </div>
-    </div>
+    </section>
   );
 };
