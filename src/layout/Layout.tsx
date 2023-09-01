712de70457957f676344b708d997f6991b1989c1
Vladimir <noise123q@mail.ru>
2023-09-02 01:34:39 +0300

    feat: (CLOTHBERRY-41) add footer

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { Footer } from '@components/Footer/Footer';
 import { Header } from '@components/Header/Header';
 
 export const Layout: FC = () => {
@@ -10,6 +11,7 @@ export const Layout: FC = () => {
       <main className='main'>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
