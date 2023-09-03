e93b33e3e1e96006c7e145a44b608ac52fdf2904
Vladimir <noise123q@mail.ru>
2023-09-03 20:36:15 +0300

    feat: (CLOTHBERRY-41) add slider in main

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,32 @@
-import { Link } from 'react-router-dom';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import classes from './HomePage.module.css';
+import Img1 from '@assets/images/main-img-1.png';
+import Img2 from '@assets/images/main-img-2.jpg';
+
+import style from './HomePage.module.css';
 
 export const HomePage = () => {
   return (
-    <div className={classes.home}>
-      <div className={classes.links}>
-        <Link className={classes['home-btn']} to='/signin'>
-          Sign in
-        </Link>
-        <Link className={classes['home-btn']} to='/signup'>
-          Sign up
-        </Link>
+    <div className={style.home}>
+      <div className={style['home-wrapper']}>
+        <Swiper
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className='mySwiper3'
+        >
+          <SwiperSlide>
+            <img src={Img1} alt='pic1' height='100%' width='100%' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Img2} alt='pic2' height='100%' width='100%' />
+          </SwiperSlide>
+        </Swiper>
       </div>
-      <h1>Main page</h1>
     </div>
   );
 };
