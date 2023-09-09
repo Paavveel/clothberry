a6ebeaf805f7b45dda55cd8e62e43d4c33473e02
Paavveel <pa.tomilin@gmail.com>
2023-09-09 21:44:01 +0300

    feat: (CLOTHBERRY-57) implement render line items

--- src/components/BasketList/BasketList.tsx
+++ src/components/BasketList/BasketList.tsx
@@ -1,11 +1,49 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 
 import { BasketItem } from '@components/BasketItem/BasketItem';
+import { updateCart } from '@store/features/auth/cartApi';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
 
 import { Button } from '..';
 import styles from './BasketList.module.css';
 
 export const BasketList: FC = () => {
+  const cart = useAppSelector((state) => state.auth.cart);
+  const lineItems = useAppSelector((state) => state.auth.cart?.lineItems);
+  const dispatch = useAppDispatch();
+
+  const handleUpdateItem = useCallback(
+    async (lineItemId: string, quantity: number) => {
+      if (cart) {
+        await dispatch(
+          updateCart({
+            cartId: cart.id,
+            body: {
+              version: cart.version,
+              actions: [{ action: 'changeLineItemQuantity', lineItemId, quantity }],
+            },
+          })
+        ).unwrap();
+      }
+    },
+    [cart, dispatch]
+  );
+  const handleDeleteItem = useCallback(
+    async (lineItemId: string) => {
+      if (cart) {
+        await dispatch(
+          updateCart({
+            cartId: cart.id,
+            body: {
+              version: cart.version,
+              actions: [{ action: 'removeLineItem', lineItemId }],
+            },
+          })
+        ).unwrap();
+      }
+    },
+    [cart, dispatch]
+  );
   return (
     <div className={styles.basket__wrapper}>
       <div className={styles.basket__table}>
@@ -15,22 +53,34 @@ export const BasketList: FC = () => {
           <span className={styles.quantity__header}>Quantity</span>
           <span className={styles.amount__header}>Amount Payable</span>
         </div>
-        <BasketItem title='Nike Air VaporMax 2023 Flyknit' price={801} />
-        <BasketItem title='Nike Air VaporMax 2023 Flyknit' price={801} />
-        <BasketItem title='Nike Air VaporMax 2023 Flyknit' price={801} />
-        <BasketItem title='Nike Air VaporMax 2023 Flyknit' price={801} />
+        {lineItems &&
+          lineItems.map((item) => (
+            <BasketItem
+              key={item.id}
+              item={item}
+              handleUpdateItem={handleUpdateItem}
+              handleDeleteItem={handleDeleteItem}
+            />
+          ))}
       </div>
       <div className={styles.basket__order}>
         <h2>Total Price</h2>
         <p className={styles.basket__order__price}>
           Amount
           <span className={styles.basket__order__dotted} />
-          36829р
+          {cart ? cart.totalPrice.centAmount / 100 : 0}$
         </p>
-        <input type='text' className={styles.basket__order__promocode__input} placeholder='Promocode' maxLength={15} />
-        <Button type='button' className={styles.apply__promocode__button} secondary>
-          Apply code
-        </Button>
+        <div className={styles.promo_wrapper}>
+          <input
+            type='text'
+            className={styles.basket__order__promocode__input}
+            placeholder='Promocode'
+            maxLength={15}
+          />
+          <Button type='button' className={styles.apply__promocode__button} secondary>
+            Apply code
+          </Button>
+        </div>
 
         <Button type='button' className={styles.buy} primary>
           Сheckout
