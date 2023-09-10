6bd3926889b1fc693e5f23863a9882941465c10e
Vladimir <noise123q@mail.ru>
2023-09-10 23:18:42 +0300

    feat: (CLOTHBERRY-51) add content about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,5 +1,42 @@
+import dev2 from '@assets/images/alexey.jpg';
+import dev1 from '@assets/images/pavel.jpeg';
+import { Breadcrumbs } from '@components/Breadcrumbs/Breadcrumbs';
+
 import style from './AboutPage.module.css';
 
 export const AboutPage = () => {
-  return <div className={style.title}>AboutPage</div>;
+  return (
+    <section>
+      <Breadcrumbs />
+      <h1>About us</h1>
+      <p>
+        This project was implemented by a team of students from rsschool 2023Q1 Three React Ninjas. The team
+        communicated via discord. The project was maintained in github projects.
+      </p>
+      <div className={style.creator}>
+        <div className={style['creator-img']}>
+          <img src={dev1} alt='pavel' width='100%' height='100%' />
+        </div>
+        <span className={style['creator-title']}>Pavel, Team Leader</span>
+        <p className={style['creator-description']}>
+          Pavel realized login page, user profile, basket page, write a lot of tests, and more useful things
+        </p>
+        <a className={style['creator-link']} href='https://github.com/Paavveel' target='_blank' rel='noreferrer'>
+          Github
+        </a>
+      </div>
+      <div className={style.creator}>
+        <div className={style['creator-img']}>
+          <img className={style['creator-img-alexey']} src={dev2} alt='pavel' width='100%' height='100%' />
+        </div>
+        <span className={style['creator-title']}>Alexey, left-hand Team-Leader</span>
+        <p className={style['creator-description']}>
+          Alexey realized catalog page, registration page, basket page, write some tests, and more useful things
+        </p>
+        <a className={style['creator-link']} href='https://github.com/dedushkaalex' target='_blank' rel='noreferrer'>
+          Github
+        </a>
+      </div>
+    </section>
+  );
 };
