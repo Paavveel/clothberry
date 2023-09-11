024f88d323705bd6ba3d32637e0878ec34cbb48e
Vladimir <noise123q@mail.ru>
2023-09-11 20:28:11 +0300

    feat: (CLOTHBERRY-51) add link rsschool

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,7 @@
 import dev2 from '@assets/images/alexey.jpg';
 import dev1 from '@assets/images/pavel.jpeg';
 import dev3 from '@assets/images/vladimir.jpg';
+import { ReactComponent as School } from '@assets/img/school.svg';
 import { Breadcrumbs } from '@components/Breadcrumbs/Breadcrumbs';
 
 import style from './AboutPage.module.css';
@@ -9,10 +10,15 @@ export const AboutPage = () => {
   return (
     <section>
       <Breadcrumbs />
-      <h1>About us</h1>
-      <p>
-        This project was implemented by a team of students from rsschool 2023Q1 Three React Ninjas. The team
-        communicated via discord. The project was maintained in github projects.
+      <h1 className={style.title}>About us</h1>
+      <p className={style.about}>
+        <span className={style.school}>
+          This project was implemented by a team of students from
+          <a href='https://rs.school/' target='_blank' rel='noreferrer'>
+            <School width='70' height='70' />
+          </a>
+        </span>
+        The team communicated via discord. The project was maintained in github projects.
       </p>
       <div className={style.creator}>
         <div className={style['creator-img']}>
@@ -30,7 +36,7 @@ export const AboutPage = () => {
         <div className={style['creator-img']}>
           <img className={style['creator-img-alexey']} src={dev2} alt='alexey' width='100%' height='100%' />
         </div>
-        <span className={style['creator-title']}>Alexey, left-hand Team-Leader</span>
+        <span className={style['creator-title']}>Alexey, right-hand Team-Leader</span>
         <p className={style['creator-description']}>
           Alexey realized catalog page, registration page, basket page, write some tests and more useful things
         </p>
@@ -42,9 +48,9 @@ export const AboutPage = () => {
         <div className={style['creator-img']}>
           <img src={dev3} alt='vladimir' width='100%' height='100%' />
         </div>
-        <span className={style['creator-title']}>Vladimir, right-hand Team-Leader</span>
+        <span className={style['creator-title']}>Vladimir, left-hand Team-Leader</span>
         <p className={style['creator-description']}>
-          Vladimir realized product page, about page, main page and more useful things
+          Vladimir realized product page, about page, main page and some useful things
         </p>
         <a className={style['creator-link']} href='https://github.com/noisekov' target='_blank' rel='noreferrer'>
           Github
