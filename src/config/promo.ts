ab5231d27a99290ecc4ab207d12bb0db5c82ec45
Paavveel <pa.tomilin@gmail.com>
2023-09-12 14:20:57 +0300

    feat: (CLOTHBERRY-57) implement Promo codes

---
+++ src/config/promo.ts
@@ -0,0 +1,5 @@
+export const availablePromo = [
+  { label: '15% discount for order', code: 'SALE15' },
+  { label: '50% discount for order', code: 'SALE50' },
+  { label: '80% discount for order', code: 'SALE80' },
+];
